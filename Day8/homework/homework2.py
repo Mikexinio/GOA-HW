1bceccdc23d6a0de27eaee75875766aaec4e459d
Mikexinio <136114189+Mikexinio@users.noreply.github.com>
2024-03-20 21:50:12 +0400

    Add files via upload

---
+++ Day8/homework/homework2.py
@@ -0,0 +1,4 @@
+a =int(input('enter your age: '))
+user1 =int(input('how many year do you wanna go to the future: '))
+print("so your age in this year will be:")
+print(a +user1)
---
+++ Day8/homework/homework3.py
@@ -0,0 +1,6 @@
+A = str(3.2)
+B = str(9>7)
+C = "mika"
+D = str(8)
+
+print("this is float: " + A +", this is str: " + C +", this is int: " + D +" and this is bool: " + B)
